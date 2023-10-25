72c806f5356f85756c53700b7e1b976fd4601b2d
Aj! <88217322+ajaverett@users.noreply.github.com>
2023-10-24 23:56:26 -0600

    oops

---
+++ Aj/lmaoooo.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+
+with open("full_transcript.txt", 'r') as f:
+    lines = f.read()
+
+lines = lines.split("END_ENTRY")
+
+lines = [line.replace('\n', '') for line in lines]
+
+with open('output_file.txt', 'w') as file:
+    for line in lines:
+        file.write(line + '\n')
